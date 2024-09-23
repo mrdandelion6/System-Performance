cef75ef79a079fbe2eaf9bc8358186ce258ec87a
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-09-23 04:42:02 -0400

    forgot what i did but initialized some of part1b.c... still have to finish it

--- A1/part1/part1b.c
+++ A1/part1/part1b.c
@@ -1,10 +1,37 @@
 #define _GNU_SOURCE
 
-#include <x86intrin.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "part1a.h"
+#include "part1b.h"
 #include "time_util.h"
 
+// The following cache lines (in bytes) were determined by: 
+// cat /sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size  # L1d cache line size
+// cat /sys/devices/system/cpu/cpu0/cache/index2/coherency_line_size  # L2 cache line size
+// cat /sys/devices/system/cpu/cpu0/cache/index3/coherency_line_size  # L3 cache line size
+#define L1d_cache_line 64
+#define L2_cache_line 64
+#define L3_cache_line 64
+// Note that we were allowed to assume these numbers from the assignment's instructions as well.
+
+// The following CSV file is used to persist the data collected from the Cache hierarchy experiment.
+#define CSV_NAME "cache_hierachy.csv"
+
+void make_csv() {
+    // This function is invoked to create a CSV file. If the file already exists, all of its data will
+    // be wiped and a new one blank one will be created. The CSV file has the columns "size", and
+    // "latency".
+
+    FILE* file = fopen(CSV_NAME, "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(file, "size", "latency");
+}
+
+void determine_cache_hierachy()  {
+
+}
